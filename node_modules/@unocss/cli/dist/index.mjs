70d1c619b5e7d5ad7dc04bf424c586eb0482e133
lif3ng's claw <claw@lif3ng.cn>
2026-03-17 22:53:12 +0800

    chore: 添加 .gitignore，从 Git 追踪中移除 node_modules 和 dist

--- node_modules/@unocss/cli/dist/index.mjs
+++
@@ -1,3 +0,0 @@
-import { t as build } from "./src-w3MpieSE.mjs";
-
-export { build };
--- node_modules/@unocss/preset-wind4/dist/shortcuts.mjs
+++
@@ -1,7 +0,0 @@
-import { r as containerShortcuts } from "./container-BsDB8iH5.mjs";
-
-//#region src/shortcuts/index.ts
-const shortcuts = [...containerShortcuts];
-
-//#endregion
-export { shortcuts };
